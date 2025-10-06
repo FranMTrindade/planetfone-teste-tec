9a65d7dabd599f017f98f30cf8d72c60769b8169
FranMTrindade <progfrancisco@gmail.com>
2025-10-06 00:46:10 -0300

    feat: create utils

---
+++ frontend/src/utils/menuItens.tsx
@@ -0,0 +1,14 @@
+import { MessageOutlined, UploadOutlined } from '@ant-design/icons';
+
+export const baseMenuItems = [
+  {
+    key: '/chat',
+    icon: <MessageOutlined />,
+    label: 'Chat',
+  },
+  {
+    key: '/uploads',
+    icon: <UploadOutlined />,
+    label: 'Uploads',
+  },
+];
